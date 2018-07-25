4ad91396fbfd8af00cf0f4507bbdb0d0468d137b
Alysson Campbell <alyssoncampbell252@gmail.com>
2018-07-25 09:33:26 -0400

    multiple dropdown submits working

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 //keep track of score
 //listen to each button, when that particular button is clicked, compare the drop-down chosen to the
 //diet of the animal.  If it's a match, give a point
-//once all are answered give total score 
+//once all are answered give total score
 //create the questions by shuffling the animals and displaying them all in div's
 //each question is "(sound)! I am a hungry (animal name), feed me..."
 //loop through all of the questions, when questions are all answered then give score
-let score = 0;
 
-// let dietButton = document.querySelectorAll(".diet");
+document.querySelector("button").addEventListener("click", event => {
+  event.preventDefault();
+  console.log("button clicked!");
+  let score = 0;
+  let foodChoices = [];
+  let animals = document.querySelectorAll(".animal");
+  console.log(animals);
+  for (let i = 0; i < animals.length; i++) {
+    let chosenFood = animals[i].lastElementChild.value;
+    console.log(chosenFood);
+  }
 
-guessButtons = document.querySelectorAll("submit")
-
-// for (let i = 0; i < guessButtons.length; i++) {
-//   addEventListener("click", evt => {
-//     console.log("button clicked!")
-//   })
-// }
-guessButtons.addEventListener("click", evt => {
-  console.log(this.guessButtons.value);
-})
+  // console.log(food[1].value);
+});
