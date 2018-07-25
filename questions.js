b89546b562229b2c5c44459efb6dea51eabd7907
Alysson Campbell <alyssoncampbell252@gmail.com>
2018-07-25 15:59:06 -0400

    cleaned up code

--- questions.js
+++ questions.js
@@ -1,14 +1,4 @@
-//place for the questions and answers to be listed out and then called within the script.js
-//make an object array for each animal with if they're an omnivore, carnivore or herbivore
-//make an array of answers of omnivore, carnivore or herbivore (meat, plants or meat & plants)
-
-// object = animal {
-//   name: "name",
-//   sound: "sound it makes"
-//   diet: "omnivore, herbivore or carnivore",
-//   picture: "picture.jpg"
-// }
-
+//object for answer checking & storing info
 let animals = [{
     name: "lion",
     diet: "carnivore",
